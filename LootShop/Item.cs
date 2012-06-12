32517233114203d0c6f378f9b3c68ed46b01b250
Adam Rezich <adam@rezich.com>
2012-06-12 04:24:39 -0500

    added a metric fuckton of item name lols
    no seriously, it's amazing

--- LootShop/Item.cs
+++ LootShop/Item.cs
@@ -7,9 +7,30 @@ using System.Threading.Tasks;
 
 namespace LootShop {
 	public class Item {
+		public static List<string> PreAdjectives = new List<string> {
+			"Fantastic", "Amazing", "Fuckin' Metal", "Preposterous", "Ridiculously Overpowered", "Slithering", "Rock-Hard", "Aching", "Throbbing", "Tedious", "Lengthy", "Fond", "Horrible", "Ghostly", "Sweaty", "Charming",
+			"Rifting", "Lofty", "Perfectly Ordinary", "Jubilant", "Lightly Seasoned", "Seasoned", "Withered", "Humiliating", "Meaningful", "Far-Fetched", "Controversial", "Stolen", "Imbued", "Bashful", "Delicate",
+			"Righteous", "Encouraging", "Baffling", "Brilliant", "Rotten", "Rotting", "Roaring", "Seamless", "Forboding", "Tormented", "Scattered", "Demeaning", "Well-Crafted", "Epic", "Unfinished", "Half-Assed",
+			"Rustic", "Rusted", "Stained", "Becoming", "Plentiful", "Breached", "Shady", "Ransacked", "Virgin", "Well-Trimmed", "Well-Taught", "Charitable", "Resplendent", "Regular-Ass", "Gaping", "Stout", "Unfortunate",
+			"Famished", "Impoverished", "Unheard-Of", "Ineffable", "Affable", "Unusable", "Super Shiny", "Gas-Operated", "Tony's", "Moist", "Bleached", "Well-Bathed", "Static", "Well-Tuned", "Unstrung", "Shoddy", "Cross-Hatched",
+			"Strange", "Unremarkable", "Scarcely Lethal", "Mildly Menacing", "Somewhat Threatening", "Uncharitable", "Notably Dangerous", "Sufficiently Lethal", "Truly Feared", "Wicked Nasty", "Totally Ordinary", "Australian", //lol tf2
+			"Glamorous", "Fabulous", "Stingy", "Unplanned", "Vaguely Erotic", "Seductive", "Burnt", "Chiseled", "Gloomy", "Shiny", "Musty", "Moldy", "Drab", "Worn", "Deflated", "Shriveled", "Spotted", "Termite-Covered", "Polished",
+			"Enrapturing", "Warped", "Kinda Shitty", "Mediocre", "Scratched", "Dried", "Thrown Out", "Discarded", "Broken", "Irrepairable", "Unbelievable", "Indispensable", "Overprepared", "Succulent", "Subpar", "Asymmetrical",
+			"Negligent", "Gentleman's", "Lady-Like", "Manly", "Strict", "Greedy", "Notable", "Distorted", "Sight-Seeing", "Sexy", "Painful", "Legless", "Brass", "Fallen", "Tacky", "Bundled", "Business", "Requested",
+			"Summoned", "Ripped", "Twelfth", "Poorly-Wrapped", "My Wife's", "Butlers' Choice", "Scammed", "Cascading", "Uplifting", "Drafty", "Sinking", "Golden", "Plated", "Gilded", "Loot-Worthy", "Medieval", "Tribal", "Master",
+			"Brutal", "Brütal", "Everburning", "Giggling", "Prancing", "Balanced", "Silken"
+		};
+		public static List<string> OfX = new List<string> {
+			"The Boar", "Legend", "Lore", "Christmas Past", "Biblical Proportions", "China", "The Beast", "Reckoning", "The End Times", "Poverty", "The Jews", "Unbelievable Power", "Ridicule", "Greed", "Extra Shininess", "Shit",
+			"Embarassment", "The Wildebeast", "Worth", "Note", "Hope", "Survivability", "Reminiscence", "The Tourist", "The Lion", "The Loins", "The Hawk", "The Prisoner", "Portability", "The Clockmaker", "The Fool", "Sex",
+			"Sacrifice", "Smithing", "Disaster", "Completion", "Wrath", "Ascension", "Treasure", "Snakes", "Light", "Darkness", "The Owl", "Horror", "Vegetation", "Lies", "Greatness", "Due Time", "Patience", "Uncertainty",
+			"Quality", "\"Quality\"", "Celebration", "Burning", "Giggling", "Dancing", "The Journey", "Request", "Balance", "Cleansing", "Stability", "Teaching", "Speculation", "Wizardry", "The Church"
+		};
 		public enum Type {
 			Greataxe,
 			Longsword,
+			//Staff,
+			//Bow,
 
 			Helmet,
 			Pauldrons,
@@ -43,6 +64,7 @@ namespace LootShop {
 			public Type Name;
 			public Slot Slot;
 			public List<Attribute> BaseAttributes;
+			public List<string> Names;
 
 			public static List<Item.Kind> List = new List<Item.Kind>();
 
@@ -51,19 +73,44 @@ namespace LootShop {
 				List<Attribute> armorAttr = new List<Attribute> { Attribute.Lookup(Attribute.Type.Armor) };
 				List<Attribute> pureAttr = new List<Attribute> { };
 
-				List.Add(new Item.Kind(Type.Longsword, Slot.OneHand, meleeAttr));
-				List.Add(new Item.Kind(Type.Greataxe, Slot.TwoHands, meleeAttr));
+				// Weapons
+				List.Add(new Item.Kind(Type.Longsword, Slot.OneHand, meleeAttr, new List<string> {
+					"Sword", "Rapier", "Blade", "Pointy Stick", "Cutlass", "Zweihander", "Sabre", "Falchion"
+				}));
+				List.Add(new Item.Kind(Type.Greataxe, Slot.TwoHands, meleeAttr, new List<string> {
+					"Axe", "Hatchet", "Pick", "Broad Axe", "Battleaxe", "Great Axe", "Giant Axe", "Cleaver"
+				}));
 
-				List.Add(new Item.Kind(Type.Helmet, Slot.Head, armorAttr));
-				List.Add(new Item.Kind(Type.Armor, Slot.Chest, armorAttr));
-				List.Add(new Item.Kind(Type.Pants, Slot.Legs, armorAttr));
-				List.Add(new Item.Kind(Type.Gloves, Slot.Hands, armorAttr));
-				List.Add(new Item.Kind(Type.Belt, Slot.Waist, armorAttr));
-				List.Add(new Item.Kind(Type.Boots, Slot.Feet, armorAttr));
-				List.Add(new Item.Kind(Type.Pauldrons, Slot.Shoulders, armorAttr));
+				// Armor
+				List.Add(new Item.Kind(Type.Helmet, Slot.Head, armorAttr, new List<string> {
+					"Helmet", "Hat", "Cap", "Crown", "Casque", "Basinet", "Sallet"
+				}));
+				List.Add(new Item.Kind(Type.Armor, Slot.Chest, armorAttr, new List<string> {
+					"Studded Armor", "Leather Armor", "Quilted Armor", "Ring Mail", "Scale Mail", "Chain Mail", "Breastplate", "Plate Mail", "Shirt", "Bodyshaft", "Sweater", "Rags", "Undershirt", "Robe", "Cloak"
+				}));
+				List.Add(new Item.Kind(Type.Pants, Slot.Legs, armorAttr, new List<string> {
+					"Pants", "Leggings", "Kneepads", "Chaps", "Leg-Wraps"
+				}));
+				List.Add(new Item.Kind(Type.Gloves, Slot.Hands, armorAttr, new List<string> {
+					"Gloves", "Gauntlets", "Bracers", "Mitts", "Vambraces", "Graspers"
+				}));
+				List.Add(new Item.Kind(Type.Belt, Slot.Waist, armorAttr, new List<string> {
+					"Sash", "Belt", "Coil", "Girdle", "Belly-Wrap", "String"
+				}));
+				List.Add(new Item.Kind(Type.Boots, Slot.Feet, armorAttr, new List<string> {
+					"Shoes", "Boots", "Greaves", "Sandals", "Galoshes"
+				}));
+				List.Add(new Item.Kind(Type.Pauldrons, Slot.Shoulders, armorAttr, new List<string> {
+					"Shoulders", "Pauldrons", "Epaulets", "Mantle", "Shoulderpads"
+				})); ;
 
-				List.Add(new Item.Kind(Type.Ring, Slot.Finger, pureAttr));
-				List.Add(new Item.Kind(Type.Amulet, Slot.Neck, pureAttr));
+				// Pure Attributehavers
+				List.Add(new Item.Kind(Type.Ring, Slot.Finger, pureAttr, new List<string> {
+					"Ring", "Band", "Finger-Wrap"
+				}));
+				List.Add(new Item.Kind(Type.Amulet, Slot.Neck, pureAttr, new List<string> {
+					"Amulet", "Necklass", "Charm"
+				}));
 			}
 
 			public static Kind Lookup(Type type) {
@@ -74,10 +121,11 @@ namespace LootShop {
 				return kind;
 			}
 
-			public Kind(Type type, Slot slot, List<Attribute> baseAttributes) {
+			public Kind(Type type, Slot slot, List<Attribute> baseAttributes, List<string> names) {
 				Name = type;
 				Slot = slot;
 				BaseAttributes = baseAttributes;
+				Names = names;
 			}
 
 		}
@@ -94,16 +142,17 @@ namespace LootShop {
 			public Type Name;
 			public Range AttributeCount;
 			public Range AttributeModifier;
+			public bool Magic;
 
 			public static List<RarityLevel> List = new List<RarityLevel>();
 
 			public static void Initialize() {
-				List.Add(new RarityLevel(Type.Garbage,		new Range(0, 0),	new Range(0, 0.75)));
-				List.Add(new RarityLevel(Type.Normal,		new Range(0, 0),	new Range(0.75, 1.25)));
-				List.Add(new RarityLevel(Type.Magic,		new Range(2, 4),	new Range(1.25, 1.75)));
-				List.Add(new RarityLevel(Type.Rare,			new Range(3, 5),	new Range(1.5, 2.0)));
-				List.Add(new RarityLevel(Type.Legendary,	new Range(4, 6),	new Range(1.75, 2.5)));
-				List.Add(new RarityLevel(Type.Unique,		new Range(5, 7),	new Range(2.5, 3)));
+				List.Add(new RarityLevel(Type.Garbage,		new Range(0, 0),	new Range(0, 0.75),		false));
+				List.Add(new RarityLevel(Type.Normal,		new Range(0, 0),	new Range(0.75, 1.25),	false));
+				List.Add(new RarityLevel(Type.Magic,		new Range(2, 4),	new Range(1.25, 1.75),	true));
+				List.Add(new RarityLevel(Type.Rare,			new Range(3, 5),	new Range(1.5, 2.0),	true));
+				List.Add(new RarityLevel(Type.Legendary,	new Range(4, 6),	new Range(1.75, 2.5),	true));
+				List.Add(new RarityLevel(Type.Unique,		new Range(5, 7),	new Range(2.5, 3),		true));
 			}
 
 			public static RarityLevel Lookup(RarityLevel.Type type) {
@@ -118,10 +167,11 @@ namespace LootShop {
 				return Name.ToString().DeCamelCase();
 			}
 
-			public RarityLevel(Type name, Range attributeCount, Range attributeRange) {
+			public RarityLevel(Type name, Range attributeCount, Range attributeRange, bool magic) {
 				Name = name;
 				AttributeCount = attributeCount;
 				AttributeModifier = attributeRange;
+				Magic = magic;
 			}
 		}
 		public class Attribute {
@@ -192,7 +242,14 @@ namespace LootShop {
 			// Choose a random rarity
 			// TODO: Weight this shit
 			Item.RarityLevel.Type[] rarityValues = (Item.RarityLevel.Type[])Enum.GetValues(typeof(Item.RarityLevel.Type));
-			Item.RarityLevel.Type selectedRarity = rarityValues[r.Next(0, rarityValues.Length)];
+			Item.RarityLevel.Type selectedRarity = RarityLevel.Type.Normal;
+
+			// Pick a random rarity, but if the type of item is one with no base stats (ring, amulet),
+			// keep rolling until you get a "Magic" one (blue or higher)
+			do {
+				selectedRarity = rarityValues[r.Next(0, rarityValues.Length)];
+			} while (i.Variety.BaseAttributes.Count == 0 && !RarityLevel.Lookup(selectedRarity).Magic);
+
 			i.Rarity = RarityLevel.Lookup(selectedRarity);
 
 			List<Item.Attribute> attrs = new List<Item.Attribute>();
@@ -226,6 +283,13 @@ namespace LootShop {
 				i.Attributes.Add(a.Name, attrVal);
 			}
 
+			// GENERATE THE NAME!!
+			int oddsOfOfX = 3;
+			string name = "";
+			name = i.Variety.Names[r.Next(i.Variety.Names.Count - 1)];
+			name = Item.PreAdjectives[r.Next(Item.PreAdjectives.Count - 1)] + " " + name;
+			if (r.Next(0, oddsOfOfX) == 1) name += " of " + Item.OfX[r.Next(Item.OfX.Count - 1)];
+			i.Name = name;
 			return i;
 		}
 		public static ConsoleColor RarityToConsoleColor(RarityLevel raritylevel) {
@@ -247,14 +311,10 @@ namespace LootShop {
 			}
 		}
 
-		public string Name {
-			get {
-				return "Item Name";
-			}
-		}
+		public string Name = "!!OSHIT NO NAME GENERATED!!";
 		public void WriteStatBlock() {
-			int width = 30;
-			int padding = 3;
+			int width = 32;
+			int padding = 6;
 			string line = new String('-', width);
 			Console.WriteLine(line);
 			Console.ForegroundColor = RarityToConsoleColor(Rarity);
