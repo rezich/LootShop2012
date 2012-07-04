c12f5479d0b489a1494d1a2521e77daab505cf05
Adam Rezich <adam@rezich.com>
2012-07-04 01:46:05 -0500

    further stealing from the XNA example

--- LootShop/LootShop/LootShop.cs
+++ LootShop/LootShop/LootShop.cs
@@ -28,7 +28,9 @@ namespace LootShop {
 
 			Components.Add(screenManager);
 
-			screenManager.AddScreen(new GameplayScreen(), PlayerIndex.One); // TODO: make PlayerIndex null so the main menu can determine the player or whatever
+			//screenManager.AddScreen(new GameplayScreen(), PlayerIndex.One); // TODO: make PlayerIndex null so the main menu can determine the player or whatever
+			screenManager.AddScreen(new BackgroundScreen(), null);
+			screenManager.AddScreen(new MainMenuScreen(), null);
 
 		}
 
