80c33f843c32a264104ad57d02a2f7755538688f
Adam Rezich <adam@rezich.com>
2012-07-11 12:25:19 -0500

    more GenericMenu work
    including some Samuel L. Ipsum

--- LootShop/LootShop/ScreenManager/GenericMenu.cs
+++ LootShop/LootShop/ScreenManager/GenericMenu.cs
@@ -12,11 +12,11 @@ namespace LootShop {
 		public string Title;
 		public string Description;
 		public bool Cancelable = true;
+		public bool HasContent = true;
+		public TextBlock Content = null;
 		private List<Entry> entries = new List<Entry>();
 
 		private int selectedIndex = 0;
-
-		public Vector2 Origin = Vector2.Zero;
 
 		public override void Update(GameTime gameTime) {
 			foreach (Entry e in entries) e.IsSelected = false;
@@ -41,6 +41,9 @@ namespace LootShop {
 				if (selectedIndex < 0) selectedIndex += entries.Count;
 			}
 
+			if (entries[selectedIndex].Content != null) Content = entries[selectedIndex].Content;
+			else Content = null;
+
 			PlayerIndex playerIndex;
 			if (input.IsMenuSelect(ControllingPlayer, out playerIndex)) {
 				OnSelectEntry(selectedIndex, playerIndex);
@@ -51,11 +54,29 @@ namespace LootShop {
 		}
 
 		public override void Draw(GameTime gameTime) {
-			ScreenManager.SpriteBatch.Begin();
-			ScreenManager.SpriteBatch.Draw(LootShop.CurrentGame.Pixel, new Rectangle(0, 0, ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height), new Color(0f, 0f, 0f, 0.85f));
-			for (int i = 0; i < entries.Count; i++) {
-				entries[i].Draw(ScreenManager.SpriteBatch, gameTime, Origin, i);
+			float entriesHeight = 0;
+			foreach (Entry e in entries) {
+				entriesHeight += e.Height;
 			}
+			Vector2 entriesOrigin = new Vector2(8, (ScreenManager.GraphicsDevice.Viewport.Height / 2) - (entriesHeight / 2));
+			Vector2 titleOrigin = HasContent ? new Vector2((ScreenManager.GraphicsDevice.Viewport.Width / 3) * 2, 8) : new Vector2((ScreenManager.GraphicsDevice.Viewport.Width / 2), 8);
+			Rectangle contentRect = new Rectangle((ScreenManager.GraphicsDevice.Viewport.Width / 3), 60, (ScreenManager.GraphicsDevice.Viewport.Width / 3) * 2 - 40, 550);
+
+			ScreenManager.SpriteBatch.Begin();
+
+			ScreenManager.SpriteBatch.Draw(LootShop.CurrentGame.Pixel, new Rectangle(0, 0, ScreenManager.GraphicsDevice.Viewport.Width, ScreenManager.GraphicsDevice.Viewport.Height), new Color(0f, 0f, 0f, 0.85f));
+
+			ScreenManager.SpriteBatch.DrawString(Font, Title, titleOrigin, Color.White, 0.0f, new Vector2(Font.MeasureString(Title).X / 2, 0), 1.0f, SpriteEffects.None, 0.0f);
+
+			if (HasContent) {
+				ScreenManager.SpriteBatch.Draw(LootShop.CurrentGame.Pixel, contentRect, Color.DarkOliveGreen);
+				if (Content != null) Content.Draw(ScreenManager.SpriteBatch, LootShop.CurrentGame.UIFontSmall, new Vector2(contentRect.X, contentRect.Y), TextBlock.TextAlign.Left, contentRect.Width);
+			}
+
+			for (int i = 0; i < entries.Count; i++) {
+				entries[i].Draw(ScreenManager.SpriteBatch, gameTime, entriesOrigin, i);
+			}
+
 			ScreenManager.SpriteBatch.End();
 		}
 
@@ -69,17 +90,23 @@ namespace LootShop {
 			OnCancel(e.PlayerIndex);
 		}
 
+		public static SpriteFont Font {
+			get { return LootShop.CurrentGame.UIFontMedium; }
+		}
+
 		public GenericMenu(string menuTitle) {
 			Title = menuTitle;
 		}
 
-		public GenericMenu(string menuTitle, bool cancelable) {
+		public GenericMenu(string menuTitle, bool cancelable, bool hasContent) {
 			Title = menuTitle;
 			Cancelable = cancelable;
+			HasContent = hasContent;
 		}
 
 		public class Entry {
 			public string Text;
+			public TextBlock Content;
 			public bool IsSelected = false;
 
 			public event EventHandler<PlayerIndexEventArgs> Selected;
@@ -89,12 +116,20 @@ namespace LootShop {
 					Selected(this, new PlayerIndexEventArgs(playerIndex));
 			}
 
+			public float Height {
+				get { return Font.LineSpacing; }
+			}
+
 			public void Draw(SpriteBatch spriteBatch, GameTime gameTime, Vector2 origin, int position) {
-				spriteBatch.DrawString(LootShop.CurrentGame.UIFontSmall, Text, origin + new Vector2(0, position * 20), IsSelected ? Color.White : Color.Gray);
+				spriteBatch.DrawString(GenericMenu.Font, Text, origin + new Vector2(0, position * GenericMenu.Font.LineSpacing), IsSelected ? Color.White : Color.Gray);
 			}
 
 			public Entry(string text) {
 				Text = text;
+			}
+			public Entry(string text, TextBlock content) {
+				Text = text;
+				Content = content;
 			}
 		}
 	}
--- LootShop/LootShop/Screens/TestMenu.cs
+++ LootShop/LootShop/Screens/TestMenu.cs
@@ -7,11 +7,22 @@ namespace LootShop {
 	public class TestMenu : GenericMenu {
 		public TestMenu()
 			: base("Test Menu") {
-				Entry itemOne = new Entry("Test");
-				Entry itemTwo = new Entry("Test 2");
+				Entry itemOne = new Entry("Spawn another menu!");
+				Entry itemTwo = new Entry("Test 2", new TextBlock("The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. #NL# #NL# Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. #NL# #NL# Look, just because I don't be givin' no man a foot massage don't make it right for Marsellus to throw Antwone into a glass motherfuckin' house, fuckin' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, 'cause I'll kill the motherfucker, know what I'm sayin'? #NL# #NL# Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit? It actually says that in the little book that comes with it: the most popular gun in American crime. Like they're actually proud of that shit. Also, press the #X_BUTTON# to do absolutely fucking nothing."));
+
+				itemOne.Selected += SpawnAnotherMenu;
 
 				MenuEntries.Add(itemOne);
 				MenuEntries.Add(itemTwo);
+				MenuEntries.Add(new Entry("Test filler"));
+				MenuEntries.Add(new Entry("Test filler"));
+				MenuEntries.Add(new Entry("Test filler"));
+				MenuEntries.Add(new Entry("Test filler"));
+				MenuEntries.Add(new Entry("Test filler"));
+		}
+
+		void SpawnAnotherMenu(object sender, PlayerIndexEventArgs e) {
+			ScreenManager.AddScreen(new TestMenu(), ControllingPlayer);
 		}
 	}
 }
